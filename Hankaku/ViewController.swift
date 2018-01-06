eb57697a9c701ec544650c905fbf50bb5d19dda4
colorbox <colorbox222@gmail.com>
2018-01-06 19:01:29 +0900

    Add copy and paste function

--- Hankaku/ViewController.swift
+++ Hankaku/ViewController.swift
@@ -2,8 +2,27 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
+
     @IBAction func convertHankaku(_ sender: Any) {
         textView.text = convert(target: textView.text)
+    }
+
+    @IBAction func pasteCopied(_ sender: Any) {
+        let pasteBoard = UIPasteboard.general
+
+        if let data: Data = pasteBoard.data(forPasteboardType: "public.text"),
+            let str = String(data: data, encoding: .utf8),
+            let text = textView.text {
+            textView.text = text + str
+        }
+    }
+
+    @IBAction func copyWord(_ sender: Any) {
+        let pasteBoard = UIPasteboard.general
+
+        if let data: Data = textView.text.data(using: .utf8) {
+            pasteBoard.setData(data, forPasteboardType: "public.text")
+        }
     }
 
     // swiftlint:disable function_body_length
